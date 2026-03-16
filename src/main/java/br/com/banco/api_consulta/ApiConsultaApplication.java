b32ee4d3c19137b213861e835e0c8d5cc90c9ea5
Michele Oliveira <shellyguns@gmail.com>
2026-03-15 23:41:25 -0300

    feat: projeto inicial api-consulta

---
+++ src/main/java/br/com/banco/api_consulta/ApiConsultaApplication.java
@@ -0,0 +1,13 @@
+package br.com.banco.api_consulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/banco/api_consulta/ApiConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.banco.api_consulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
